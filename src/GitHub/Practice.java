c2c6782ab3ad496657778a58f1092698f51d9057
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-09 16:43:30 -0600

    Update Practice.java

--- src/GitHub/Practice.java
+++ src/GitHub/Practice.java
@@ -4,5 +4,7 @@ public class Practice {
     public static void main(String[] args) {
         int first = 0;
         int newValue = 0;
+        double lastValue = 10 * 10;
+        System.out.println(lastValue);
     }
 }
