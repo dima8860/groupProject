ac92b8ac99f4c9c728169ecc302e1bd71818d6f4
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-10 15:33:48 -0600

    add

--- src/GitHub/MasterAdded.java
+++ src/GitHub/MasterAdded.java
@@ -4,5 +4,6 @@ public class MasterAdded {
     public static void main(String[] args) {
         System.out.println("Master Added");
         System.out.println("Master Added 2");
+        System.out.println("Master added 3");
     }
 }
