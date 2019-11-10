14d43b724822a332c64df35a2507e2dbb9915a1e
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-10 15:24:04 -0600

    Master 2

--- src/GitHub/MasterAdded.java
+++ src/GitHub/MasterAdded.java
@@ -3,5 +3,6 @@ package GitHub;
 public class MasterAdded {
     public static void main(String[] args) {
         System.out.println("Master Added");
+        System.out.println("Master Added 2");
     }
 }
