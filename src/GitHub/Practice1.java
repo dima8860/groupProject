94cdceb5bafd1564909d4ce06231764170ddafb4
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-10 11:52:51 -0600

    Update Practice1.java

--- src/GitHub/Practice1.java
+++ src/GitHub/Practice1.java
@@ -3,5 +3,6 @@ package GitHub;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("This is practice1");
+        System.out.println("This is practice11");
     }
 }
