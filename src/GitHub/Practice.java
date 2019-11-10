cbfe17215c6f300afe25b3ca14d4f18efde66734
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-10 12:55:40 -0600

    added new class

---
+++ src/GitHub/Practice.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+public class Practice {
+    public static void main(String[] args) {
+        int first = 0;
+        int newValue = 0;
+        double lastValue = 10 * 10;
+        System.out.println(lastValue);
+        System.out.println("This is a last statement");
+    }
+}
