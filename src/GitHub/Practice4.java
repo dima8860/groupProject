282adae3328245f6243fdfa7d7bb62b927b33248
Dmytro Chernetskyi <56329286+dima8860@users.noreply.github.com>
2019-11-10 15:07:07 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/GitHub/Practice3.java

---
+++ src/GitHub/Practice4.java
@@ -0,0 +1,10 @@
+package GitHub;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        System.out.println("Practice4");
+        System.out.println("Practice4");
+        System.out.println("Practice4");
+        System.out.println("Practice4");
+    }
+}
